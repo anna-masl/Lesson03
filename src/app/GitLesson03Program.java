3e2e3a36fcb9a8292af44f16b0670fc3676014b7
anna-masl <anna.maslinnikova@gmail.com>
2024-09-20 21:04:24 +0300

    add

--- src/app/GitLesson03Program.java
+++ src/app/GitLesson03Program.java
@@ -3,5 +3,6 @@ package app;
 public class GitLesson03Program {
     public static void main(String[] args){
         System.out.println("Hello git");
+        System.out.println("Hello 2 git");
     }
 }
